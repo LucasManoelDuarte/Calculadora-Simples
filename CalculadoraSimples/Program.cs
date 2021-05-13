ed0b7e6832663d875b74b7ef48577963b9503c96
LucasManoelDuarte <84089272+LucasManoelDuarte@users.noreply.github.com>
2021-05-13 15:49:58 -0300

    Add files via upload

---
+++ CalculadoraSimples/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CalculadoraSimples
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var continuar = "s";
+
+            while (continuar == "s")
+            {
+                Console.Clear();
+
+                Console.WriteLine("------ Calculadora Simples ------");
+                Console.WriteLine();
+
+                Console.Write("Digite o primeiro valor: ");
+                var primeiroValor = Console.ReadLine();
+                double primeiroValorDouble;
+                if (!double.TryParse(primeiroValor, out primeiroValorDouble))
+                {
+                    Console.WriteLine("O valor que você acabou de digitar não foi aceito. O programa irá entender como zero!!!");
+                    Console.WriteLine();
+                }
+
+                Console.Write("Digite o segundo valor: ");
+                var segundoValor = Console.ReadLine();
+                double segundoValorDouble;
+                if (!double.TryParse(segundoValor, out segundoValorDouble))
+                {
+                    Console.WriteLine("O valor que você acabou de digitar não foi aceito. O programa irá entender como zero!!!");
+                    Console.WriteLine();
+                }
+
+                Console.Write("Digite (1) para Somar, (2) para Subtrair, (3) para Multiplicar ou (4) para Dividir: ");
+                var operacao = Console.ReadLine();
+                var operacaoInt = int.Parse(operacao);
+                Console.WriteLine();
+
+                try
+                {
+                    RealizarOperacao(operacaoInt, primeiroValorDouble, segundoValorDouble);
+                }
+                catch (DivideByZeroException ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine();
+                Console.Write("Digite (s) para realizar outra conta ou qualquer outra tecla para sair: ");
+                continuar = Console.ReadLine();
+            }
+
+            Console.Clear();
+            Console.WriteLine("Aplicação finalizada. Pressione qualquer tecla para sair...");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CalculadoraSimples/RealizarOperacao.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraSimples
+{
+    partial class Program
+    {
+        static void RealizarOperacao(int operacao, double valorUm, double valorDois)
+        {
+            if (operacao == 1)
+            {
+                Somar(valorUm, valorDois);
+            }
+
+            if (operacao == 2)
+            {
+                Subtrair(valorUm, valorDois);
+            }
+
+            if (operacao == 3)
+            {
+                Multiplicar(valorUm, valorDois);
+            }
+
+            if (operacao == 4)
+            {
+                Dividir(valorUm, valorDois);
+            }
+        }
+
+        static double Somar(double valorUm, double valorDois)
+        {
+            var resultado = valorUm + valorDois;
+            Console.WriteLine($"A soma entre {valorUm} e {valorDois} é: {resultado}");
+            return resultado;
+        }
+
+        static double Subtrair(double valorUm, double valorDois)
+        {
+            var resultado = valorUm - valorDois;
+            Console.WriteLine($"A subtração entre {valorUm} e {valorDois} é: {resultado}");
+            return resultado;
+        }
+
+        static double Multiplicar(double valorUm, double valorDois)
+        {
+            var resultado = valorUm * valorDois;
+            Console.WriteLine($"A multiplicação entre {valorUm} e {valorDois} é: {resultado}");
+            return resultado;
+        }
+
+        static double Dividir(double valorUm, double valorDois)
+        {
+            if (valorDois == 0)
+            {
+                throw new DivideByZeroException($"Impossível dividir por zero!!! {valorUm} / {valorDois} Não é possível...");
+            }
+            var resultado = valorUm / valorDois;
+            Console.WriteLine($"A divisão entre {valorUm} e {valorDois} é: {resultado}");
+            return resultado;
+        }
+    }
+}
